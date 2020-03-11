fcc54c4e46306eb99f0d71938a3b0398b246f927
thomas toan <toaney@gmail.com>
2020-03-11 09:13:58 -0700

    add initial elm pairing files

---
+++ src/Main.elm
@@ -0,0 +1,35 @@
+module Main exposing (main)
+
+import Html
+import Browser
+
+-- MODEL
+type alias Model = Int
+
+init : Model 
+init = 
+    1
+
+
+-- UPDATE
+
+type Msg = Noop
+update msg model =
+    model
+
+
+
+
+-- VIEW
+
+view model = 
+    Html.text ("Hello World! " ++ (String.fromInt model))
+
+
+
+
+
+
+
+main = 
+    Browser.sandbox { init = init, update = update, view = view }
